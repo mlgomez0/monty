b52477ce106e2bfbe07001f99bba7322dd4e612f
mlgomez0 <mlgomez230@hotmail.com>
2020-01-05 13:28:42 +0000

    Fixing main return and reduced delimiter

--- bytecodes/00.m
+++ bytecodes/00.m
@@ -1,12 +1,4 @@
-push 0  hoalal
+
 push 1
-push 2
-  push 3
-                       pall
-		     push 4
-		         push 5
-                                                                                                 push 2
-
-
-			       push    6        $
-			       pall This is the end of our program. Monty is awesome!
+       push 3
+       pall hola
